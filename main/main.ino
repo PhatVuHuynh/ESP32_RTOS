b715e9e7b0e8a96598d2ca680bd8e096cfde51e0
Lam Phat <phat.vuhuynh@hcmut.edu.vn>
2024-10-07 15:48:46 +0700

    show temp, humid, moisture, light on web but not real time

--- main/main.ino
+++ main/main.ino
@@ -1,31 +1,39 @@
 #include <Adafruit_NeoPixel.h>
 #include <DHT20.h>
-#include <LiquidCrystal_I2C.h>;
+#include <LiquidCrystal_I2C.h>
 
-// #include <WiFi.h>
+#include <WiFi.h>
+#include <WebServer.h>
 
 // #include "Adafruit_MQTT.h"
 // #include "Adafruit_MQTT_Client.h"
 
 
 
-#define WLAN_SSID "abcd"
-#define WLAN_PASS "12345678"
-#define AIO_SERVER      "io.adafruit.com"
-#define AIO_SERVERPORT  1883
-#define AIO_USERNAME    "NPNLab_BBC"
-#define AIO_KEY         ""
+#define WLAN_SSID "RNM esp sida"
+#define WLAN_PASS "whyarewestillhere"
 
-#define OHS_SERVER      "mqtt.ohstem.vn"
-#define OHS_SERVERPORT  1883
-#define OHS_USERNAME    "abcd0386433465"
-#define OHS_KEY         "12345678"
+IPAddress local_ip(192,168,1,1);
+IPAddress gateway(192,168,1,1);
+IPAddress subnet(255,255,255,0);
+
+WebServer server(80);
+
+// #define AIO_SERVER      "io.adafruit.com"
+// #define AIO_SERVERPORT  1883
+// #define AIO_USERNAME    "NPNLab_BBC"
+// #define AIO_KEY         ""
+
+// #define OHS_SERVER      "mqtt.ohstem.vn"
+// #define OHS_SERVERPORT  1883
+// #define OHS_USERNAME    "abcd0386433465"
+// #define OHS_KEY         "12345678"
 
 
-#define BKTK_SERVER      "mqttserver.tk"
-#define BKTK_SERVERPORT  1883
-#define BKTK_USERNAME    "innovation"
-#define BKTK_KEY         "Innovation_RgPQAZoA5N"
+// #define BKTK_SERVER      "mqttserver.tk"
+// #define BKTK_SERVERPORT  1883
+// #define BKTK_USERNAME    "innovation"
+// #define BKTK_KEY         "Innovation_RgPQAZoA5N"
 
 // WiFiClient client;
 // //Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_USERNAME, AIO_KEY);
@@ -59,17 +67,15 @@ Adafruit_NeoPixel pixels3(4, D7, NEO_GRB + NEO_KHZ800);
 DHT20 dht20;
 LiquidCrystal_I2C lcd(33,16,2);
 
+// void slidercallback(double x) {
+//   Serial.print("Hey we're in a slider callback, the slider value is: ");
+//   Serial.println(x);
+// }
 
-
-void slidercallback(double x) {
-  Serial.print("Hey we're in a slider callback, the slider value is: ");
-  Serial.println(x);
-}
-
-void onoffcallback(char *data, uint16_t len) {
-  Serial.print("Hey we're in a onoff callback, the button value is: ");
-  Serial.println(data);
-}
+// void onoffcallback(char *data, uint16_t len) {
+//   Serial.print("Hey we're in a onoff callback, the button value is: ");
+//   Serial.println(data);
+// }
 
 // void MQTT_connect() {
 //   int8_t ret;
@@ -105,10 +111,16 @@ void setup() {
   dht20.begin();
   lcd.begin();
   pixels3.begin();
+  // pinMode(LED_BUILTIN, OUTPUT);
+  // Serial.println("123");
 
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
 
+  WiFi.softAP(WLAN_SSID, WLAN_PASS);
+  WiFi.softAPConfig(local_ip, gateway, subnet);
+  // WiFi.mode(WIFI_STA);
   // WiFi.begin(WLAN_SSID, WLAN_PASS);
-  // delay(2000);
 
   // while (WiFi.status() != WL_CONNECTED) {
   //   delay(500);
@@ -117,6 +129,22 @@ void setup() {
   // Serial.println();
   // Serial.print("IP address: ");
   // Serial.println(WiFi.localIP());
+  delay(100);
+
+  // Serial.print("IP address: ");
+  // Serial.println(WiFi.localIP());
+  // Serial.println("wifi");
+
+  server.on("/", handle_Onconnect);
+  // server.on("/t", handle_temp_humid);
+  server.on("/relayOn", handle_relayOn);
+  server.on("/relayOff", handle_relayOff);
+  // server.on("/l", handle_light);
+  // server.on("/ledon", handle_ledOn);
+  // server.on("/ledoff", handle_ledOff);
+  server.onNotFound(handle_NotFound);
+  server.begin();
+  Serial.println("Server Started.");
 
   // slider.setCallback(slidercallback);
   // onoffbutton.setCallback(onoffcallback);
@@ -136,8 +164,24 @@ void setup() {
   
   
 }
-int pubCount = 0;
+// int pubCount = 0;
+float temp = 0.0;
+float humid = 0.0;
+uint16_t moist = 0;
+uint16_t lightRes = 0;
+
+// bool LED_BUILTIN_STATUS = LOW;
+bool RELAY_STATUS = LOW;
+bool RGB_STATUS = LOW;
+
 void loop() {
+  server.handleClient();
+  // if(LED_BUILTIN_STATUS){
+  //   digitalWrite(LED_BUILTIN, HIGH);
+  // }
+  // else{
+  //   digitalWrite(LED_BUILTIN, LOW);
+  // }
   // MQTT_connect();
   // mqtt.processPackets(10000);
   // if(! mqtt.ping()) {
@@ -145,7 +189,102 @@ void loop() {
   // }
 }
 
+String getHTML(){
+  String htmlcode = "<!DOCTYPE html> <html>\n";
+  htmlcode += "<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
+  htmlcode += "<tittle>Test</tittle>\n";
+  // htmlcode += "<script src=\"https://cdn.jsdelivr.net/npm/chart.js\"></script>";
 
+  htmlcode += "</head>\n";
+  htmlcode += "<body>\n";
+  htmlcode += "<h1>Yolo Uno Mini Server</h1>\n";
+  htmlcode += "<h3>Simple demo using AP mode</h3>\n";
+
+  String tempS = (String)temp;
+  htmlcode += "<p>Temperature: ";
+  htmlcode += tempS;
+  htmlcode += "C</p>\n";
+
+  String humidS = (String)humid;
+  htmlcode += "<p>Humid: ";
+  htmlcode += humidS;
+  htmlcode += "%</p>\n";
+
+  String moistS = String(moist * 100 / 4096);
+  htmlcode += "<p>Moist: ";
+  htmlcode += moistS;
+  htmlcode += "%</p>\n";
+
+  if(RELAY_STATUS){
+    htmlcode += "<p>Relay status: ON</p><a href=\"/ledoff\">Please turn off relay</a>\n";
+  }
+  else{
+    htmlcode += "<p>Relay status: OFF</p><a href=\"/ledon\">Please turn on relay</a>\n";
+  }
+
+  String lightResS = String(lightRes * 100 / 4096);
+  htmlcode += "<p>Light: ";
+  htmlcode += lightResS;
+  htmlcode += "%</p>\n";
+
+  if(RGB_STATUS){
+    htmlcode += "<p>RGB LED status: ON</p><a href=\"/ledoff\">Please turn off RGB LED</a>\n";
+  }
+  else{
+    htmlcode += "<p>RGB LED status: OFF</p><a href=\"/ledon\">Please turn on RGB LED</a>\n";
+  }
+
+  htmlcode += "</body>\n";
+  htmlcode += "</html>\n";
+
+  return htmlcode;
+}
+
+void handle_Onconnect(){
+  // LED_BUILTIN_STATUS = LOW;
+  // Serial.println("LED status: OFF");
+  server.send(200, "text/html", getHTML());
+}
+
+// void handle_temp_humid(){
+//   // LED_BUILTIN_STATUS = LOW;
+//   // Serial.println("LED status: OFF");
+//   server.send(200, "text/html", getHTML());
+// }
+
+void handle_relayOn(){
+  // LED_BUILTIN_STATUS = HIGH;
+  Serial.println("Relay status: ON");
+  server.send(200, "text/html", getHTML());
+}
+
+void handle_relayOff(){
+  // LED_BUILTIN_STATUS = LOW;
+  Serial.println("Relay status: OFF");
+  server.send(200, "text/html", getHTML());
+}
+
+// void handle_light(){
+//   // LED_BUILTIN_STATUS = LOW;
+//   // Serial.println("LED status: OFF");
+//   server.send(200, "text/html", getHTML());
+// }
+
+// void handle_ledOn(){
+//   // LED_BUILTIN_STATUS = HIGH;
+//   Serial.println("LED status: ON");
+//   server.send(200, "text/html", getHTML());
+// }
+
+// void handle_ledOff(){
+//   // LED_BUILTIN_STATUS = LOW;
+//   Serial.println("LED status: OFF");
+//   server.send(200, "text/html", getHTML());
+// }
+
+void handle_NotFound(){
+  server.send(404, "text/plain", "NOT FOUND");
+}
 
 /*--------------------------------------------------*/
 /*---------------------- Tasks ---------------------*/
@@ -159,7 +298,7 @@ void TaskBlink(void *pvParameters) {  // This is a task.
   pinMode(LED_BUILTIN, OUTPUT);
   
 
-  while(1) {                          
+  while(1) {    
     digitalWrite(LED_BUILTIN, HIGH);  // turn the LED ON
     delay(1000);
     digitalWrite(LED_BUILTIN, LOW);  // turn the LED OFF
@@ -175,18 +314,20 @@ void TaskTemperatureHumidity(void *pvParameters) {  // This is a task.
   //uint32_t blink_delay = *((uint32_t *)pvParameters);
 
   while(1) {                          
-    Serial.println("Task Temperature and Humidity");
+    // Serial.println("Task Temperature and Humidity");
     dht20.read();
     // Serial.println(dht20.getTemperature());
     // Serial.println(dht20.getHumidity());
-    
+    temp = dht20.getTemperature();
+    humid = dht20.getHumidity();
+
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print("Temp:");
-    lcd.print(dht20.getTemperature());
+    lcd.print(temp);
     lcd.setCursor(0, 1);
     lcd.print("Humid:");
-    lcd.print(dht20.getHumidity());
+    lcd.print(humid);
 
     delay(5000);
   }
@@ -198,34 +339,38 @@ void TaskSoilMoistureAndRelay(void *pvParameters) {  // This is a task.
   pinMode(D9, OUTPUT);
 
   while(1) {                          
-    Serial.println("Task Soild and Relay");
-    //Serial.println(analogRead(A3));
+    // Serial.println("Task Soild and Relay");
     
-    if(analogRead(A3) > 500){
+    moist = analogRead(A3);
+    // Serial.println(moist);
+    
+    if(moist > 500){
       digitalWrite(D9, LOW);
     }
-    if(analogRead(A3) < 50){
+    if(moist < 50){
       digitalWrite(D9, HIGH);
     }
     delay(1000);
   }
 }
 
-
 void TaskLightAndLED(void *pvParameters) {  // This is a task.
   //uint32_t blink_delay = *((uint32_t *)pvParameters);
 
   while(1) {                          
-    Serial.println("Task Light and LED");
-    // Serial.println(analogRead(A2));
-    if(analogRead(A2) < 350){
+    // Serial.println("Task Light and LED");
+
+    lightRes = analogRead(A2);
+    // Serial.println(lightRes);
+
+    if(lightRes < 350){
       pixels3.setPixelColor(0, pixels3.Color(255,0,0));
       pixels3.setPixelColor(1, pixels3.Color(255,0,0));
       pixels3.setPixelColor(2, pixels3.Color(255,0,0));
       pixels3.setPixelColor(3, pixels3.Color(255,0,0));
       pixels3.show();
     }
-    if(analogRead(A2) > 550){
+    if(lightRes > 550){
       pixels3.setPixelColor(0, pixels3.Color(0,0,0));
       pixels3.setPixelColor(1, pixels3.Color(0,0,0));
       pixels3.setPixelColor(2, pixels3.Color(0,0,0));
